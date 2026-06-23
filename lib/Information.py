06191844ec64cc669f9934804e3e6610e6ea860e
Saman Torabi <36084415+TisheKar@users.noreply.github.com>
2026-06-23 18:25:11 +0330

    Update Information.py

--- lib/Information.py
+++ lib/Information.py
@@ -1,10 +1,10 @@
 from .library import *
 
-admin_user_id = 123456789 #<--- آیدی ادمین
-api_id = 123456789 #<--- آی پی آی آیدی
-api_hash = 'abcdefghigklmnopqrstuvwxyz' #<--- ای پی آی هش
-helper_username = 'BotUserName' #<--- یوزر ربات هلپر بدون @
-bot_token = 'abcdefghigklmnopqrstuvwxyz:466546' #<--- توکن ربات هلپر
+admin_user_id = 8599705796 #<--- آیدی ادمین
+api_id = 33494882 #<--- آی پی آی آیدی
+api_hash = 'a85812b381be0c0f5c1e58bd79ca3509' #<--- ای پی آی هش
+helper_username = 'myselfux_bot' #<--- یوزر ربات هلپر بدون @
+bot_token = '8817276845:AAEYqLQA7Oa0FWtO2QtpAFZt3osXHYZrU5w' #<--- توکن ربات هلپر
 
 client_id = '01e7dc6b41c3471b94efe87abeb05919'
 client_secret = '4f5f93af1ced4b0d9ba8440606803639'
